d1f853cd09f28c6b8652c4c8b9ca5d00307dab5b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-02-01 12:49:59 -0300

    feat: added user pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,5 @@
 import { RouteConfig } from 'vue-router';
+import { UserRouteName, UserRoutePath } from '../enums/routes';
 
 const routes: RouteConfig[] = [
   {
@@ -6,6 +7,46 @@ const routes: RouteConfig[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
+    ],
+  },
+  {
+    path: '/user',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        name: UserRouteName.List,
+        path: UserRoutePath.List,
+        component: () => import('pages/user/List.vue'),
+      },
+      {
+        name: UserRouteName.Create,
+        path: UserRoutePath.Create,
+        meta: {
+          create: true,
+        },
+        component: () => import('pages/user/Form.vue'),
+      },
+      {
+        name: UserRouteName.Update,
+        path: UserRoutePath.Update,
+        meta: {
+          update: true,
+        },
+        component: () => import('pages/user/Form.vue'),
+      },
+      {
+        name: UserRouteName.View,
+        path: UserRoutePath.View,
+        meta: {
+          view: true,
+        },
+        component: () => import('pages/user/Form.vue'),
+      },
+      {
+        name: UserRouteName.Remove,
+        path: UserRoutePath.Remove,
+        component: () => import('pages/user/Remove.vue'),
+      },
     ],
   },
 
